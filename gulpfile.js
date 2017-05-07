f9dcc377271776d9e37586fc8305eefe1e766ac5
Hubert Nguyen <hubertanguyen@gmail.com>
2017-05-07 18:53:53 -0500

    set up sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,25 @@
 /* File: gulpfile.js */
-
+'use strict';
 // grap our gulp packages
 
-var gulp = require('gulp'),
-	gutil = require("gulp-util");
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create(); // create a browser sync instance
+
+
+gulp.task('sass', function() {
+	return gulp.src("./src/scss/**/*.scss")
+		.pipe(sass())
+		.pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('browser-sync', function(){
+	browserSync.init({
+		server: {
+			baseDir: "./src"
+		}
+	});
+});
 
 
 // create a default task and just log a message
